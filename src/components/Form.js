7fe18f6e3bac27fbfac471313d1595aa49032f0f
william <masterpkpk@gmail.com>
2021-04-09 12:32:59 -0400

    added footer, made changes to boss component state

---
+++ src/components/Form.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  
+  state = {
+    content: ""
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <form action="post">
+          
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
